60bbaf7fe15aef93af7ad7bf62700ae127807677
David Wright <davidtwright89@gmail.com>
2021-01-26 18:06:22 -0500

    Initial Commit

---
+++ HapticFeedbackSampler/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HapticFeedbackSampler
+//
+//  Created by David Wright on 1/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
