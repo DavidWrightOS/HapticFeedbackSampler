52ff25a45c373121080812c3eb4267e71e568599
David Wright <davidtwright89@gmail.com>
2021-01-26 19:47:12 -0500

    Added buttons to test out the haptic feedback generated from each of the UIFeedbackGenerator types

--- HapticFeedbackSampler/ViewController.swift
+++ HapticFeedbackSampler/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    // MARK: - UIImpactFeedbackGenerators
+    
+    @IBAction func impactFeedbackLight() { UIImpactFeedbackGenerator(style: .light).impactOccurred() }
+    @IBAction func impactFeedbackMedium() { UIImpactFeedbackGenerator(style: .medium).impactOccurred() }
+    @IBAction func impactFeedbackHeavy() { UIImpactFeedbackGenerator(style: .heavy).impactOccurred() }
+    @IBAction func impactFeedbackSoft() { UIImpactFeedbackGenerator(style: .soft).impactOccurred() }
+    @IBAction func impactFeedbackRigid() { UIImpactFeedbackGenerator(style: .rigid).impactOccurred() }
+    
+    // MARK: - UINotificationFeedbackGenerators
+    
+    @IBAction func notificationFeedbackError() { UINotificationFeedbackGenerator().notificationOccurred(.error) }
+    @IBAction func notificationFeedbackSuccess() { UINotificationFeedbackGenerator().notificationOccurred(.success) }
+    @IBAction func notificationFeedbackWarning() { UINotificationFeedbackGenerator().notificationOccurred(.warning) }
+    
+    // MARK: - UISelectionFeedbackGenerator
+    
+    let selectionFeedbackGenerator = UISelectionFeedbackGenerator()
+    
+    @IBAction func selectionFeedback() {
+        selectionFeedbackGenerator.selectionChanged()
+        selectionFeedbackGenerator.prepare()
     }
-
-
 }
-
